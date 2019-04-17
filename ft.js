9ad79cbd0f98d98ef6d4c136a5fe3669a241a242
Samuel Wilder <pebazium@gmail.com>
2019-04-17 16:18:11 -0400

    Initial Commit

---
+++ ft.js
@@ -0,0 +1,30 @@
+/*
+Goal:
+
+POST data from one static page to another static page easily.
+
+// Go to new page and give data to it
+ftPost('http://some.page.com/index.html', {'My' : 'data'})
+
+// Get data from page fragment
+ftGetFragment()
+*/
+
+
+function ftPost(page, data)
+{
+	var buffer = msgpack.encode(data);
+	var encoded = btoa(String.fromCharCode.apply(null, buffer));
+	var url = new URL(page);
+	url.hash = encoded;
+	window.location = url;
+}
+
+
+function ftGetFragment()
+{
+	var hash = window.location.hash.slice(1);
+	var buffer = Uint8Array.from(atob(hash), c => c.charCodeAt(0))
+	var data = msgpack.decode(buffer);
+	return data;
+}
